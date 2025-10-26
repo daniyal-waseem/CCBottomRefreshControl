86231ca289df2938d54edc5f079f86ae903d9fc0
daniyal-waseem <82135420+daniyal-waseem@users.noreply.github.com>
2025-10-26 14:34:50 +0000

    Fix package path and update swift-tools-version format
    Updated the package path and swift-tools-version format.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .target(
             name: "CCBottomRefreshControl",
-            path: "Example",
+            path: "Classes",
             publicHeadersPath: ".",
             cSettings: [
                 .headerSearchPath(".")
